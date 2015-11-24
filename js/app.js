a82663c204b699ff36b63fa169fef0c256ce10bf
Sally Kingston <sally.a.kingston@gmail.com>
2015-11-24 09:16:41 -0500

    more path fixes

--- js/app.js
+++ js/app.js
@@ -8,16 +8,16 @@
   .config(function($routeProvider){
     $routeProvider
         .when('/', {
-          templateUrl: '/myViews/targets/list.html',
+          templateUrl: 'views/targets/list.html',
           controller: 'TargetsController'
 
         })
         .when('/addTarget', {
-          templateUrl: '/myViews/targets/create.html',
+          templateUrl: 'views/targets/create.html',
           controller: 'TargetsController'
         })
         .when('/404', {
-          templateUrl: '/myViews/404.html'
+          templateUrl: 'views/404.html'
         })
         .otherwise({ redirectTo: '/404'})
 
